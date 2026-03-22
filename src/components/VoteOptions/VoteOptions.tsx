4c717b0288565834a0ffb1cb27001d691cd863cc
Tymofii Danilov <timadanilov2002@gmail.com>
2026-03-22 13:42:50 +0100

    small fix

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -13,7 +13,7 @@ export default function VoteOptions(props: VoteOptionsProps) {
             <button onClick={() => props.onVote("good")} className={css.button}>Good</button>
             <button onClick={() => props.onVote("neutral")} className={css.button}>Neutral</button>
             <button onClick={() => props.onVote("bad")} className={css.button}>Bad</button>
-            <button style={{ display: props.canReset ? "block" : "none" }} onClick={() => props.onReset()} className={`${css.button} ${css.reset}`}>Reset</button>
+            { props.canReset && <button onClick={() => props.onReset()} className={`${css.button} ${css.reset}`}>Reset</button> }
         </div>
     )
 }
